e282dc1e5c5cb553a165b3a3ccebebebb3984b2d
Jakub Piskorz <kpiskorz13@gmail.com>
2022-01-31 17:25:32 +0100

    third commit

--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 from blog import app
-from blog.models import Entry
+from blog.models import Entry, db
+from blog.forms import EntryForm
 
 @app.route("/")
 def index():
@@ -8,5 +9,32 @@ def index():
 
    return render_template("homepage.html", all_posts=all_posts)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+   form = EntryForm()
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           entry = Entry(
+               title=form.title.data,
+               body=form.body.data
+               is_published=form.is_published.data
+           )
+           db.session.add(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def edit_entry(entry_id):
+   entry = Entry.query.filter_by(id=entry_id).first_or_404()
+   form = EntryForm(obj=entry)
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():s
+           form.populate_obj(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
